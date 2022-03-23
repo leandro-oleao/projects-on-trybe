2905d5911626d4f0c3a325e2c25d01476394345b
Leandro Leao <leandro.leao@outlook.com>
2022-03-23 18:46:57 -0300

    Requisito 01 ok

--- index.js
+++ index.js
@@ -7,9 +7,16 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+const talkersFromFile = require('./gettalker');
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (_req, res) => {
+  const talkers = await talkersFromFile.getTalkers();
+  res.status(200).json(talkers);
 });
 
 app.listen(PORT, () => {
