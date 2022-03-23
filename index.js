06fba3c892a47c5d3a35312513376e244a17de36
Leandro Leao <leandro.leao@outlook.com>
2022-03-23 19:09:43 -0300

    Requisito 02 ok

--- index.js
+++ index.js
@@ -19,6 +19,16 @@ app.get('/talker', async (_req, res) => {
   res.status(200).json(talkers);
 });
 
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = await talkersFromFile.getTalkers();
+  const talker = talkers.find((t) => t.id === parseInt(id, 0));
+
+  if (!talker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+
+  return res.status(200).json(talker);
+});
+
 app.listen(PORT, () => {
   console.log('Online');
 });
