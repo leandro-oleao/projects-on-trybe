2b5a76713bcb17a44169fe96aa22d6fbcc4fe5c6
Leandro Leao <leandro.leao@outlook.com>
2022-03-23 21:17:18 -0300

    Requisito 03 ok

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+const crypto = require('crypto');
 const talkersFromFile = require('./gettalker');
 
 // não remova esse endpoint, e para o avaliador funcionar
@@ -27,6 +28,42 @@ app.get('/talker/:id', async (req, res) => {
   if (!talker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
 
   return res.status(200).json(talker);
+});
+
+function validateEmail(req, res, next) {
+  const { email } = req.body;
+
+  if (!email || email === '') {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+  // Validação de email atráves de Regex : https://pt.stackoverflow.com/questions/1386/express%C3%A3o-regular-para-valida%C3%A7%C3%A3o-de-e-mail
+  const emailValidation = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+
+  if (!emailValidation.test(email)) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+
+  next();
+}
+
+function validatePassword(req, res, next) {
+  const { password } = req.body;
+
+  if (!password || password === '') {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+
+  if (password.length < 6) { 
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+
+  next();
+}
+
+app.post('/login', validateEmail, validatePassword, (req, res) => {
+  const token = crypto.randomBytes(8).toString('hex');
+
+  res.status(200).json({ token });
 });
 
 app.listen(PORT, () => {
